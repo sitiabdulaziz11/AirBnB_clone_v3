b74ab99eb18a9d2eeefa3f212bb524ee0e736bfa
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2024-11-09 21:29:27 +0300

    Airbnb 5

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -13,17 +13,42 @@ from models.state import State
 def get_states():
     """Retrieves the list of all State objects.
     """ 
-    val  = storage.all(State)
-    for state in val:
-        res = state.to_dict()
-    return jsonify(res)
+    state_dicts = []
+    
+    sortedVal = sorted(storage.all(State).values(), key=lambda state: state.name)
+    
+    # for state in sortedVal:
+    #     state_dict = state.to_dict()
+    #     state_dicts.append(state_dict)
+    
+    # OR by Using List Comprehension
+    state_dicts = [state.to_dict() for state in sortedVal]
+    return jsonify(state_dicts)
 
-    states = storage.all(State)
-    return jsonify([state.to_dict() for state in states.values()])
+    # with out sorting
+    # states = storage.all(State)
+    # return jsonify([state.to_dict() for state in states.values()])
 
-@app_views.route('/states<id>', methods=['GET'], strict_slashes=False)
+@app_views.route('/states/<id>', methods=['GET'], strict_slashes=False)
 def get_states_by_id(id):
     """Retrieves the list of all State objects.
-    """ 
+    """
     val  = storage.get(State, id)
-    return jsonify(val)
+    if val is None:
+        return jsonify({"error": "Not found"}), 404
+    
+    # Convert the state to a dictionary and return it
+    state = val.to_dict()
+    return jsonify(state)
+
+@app_views.route('/states/<id>', methods=['DELETE'], strict_slashes=False)
+def delete_state(id):
+    """Deletes a State object.
+    """
+    state = storage.get(State, id)
+    if state is None:
+        return jsonify({"error": "Not found"}), 404
+    
+    storage.delete(state)
+    storage.save()
+    return jsonify({}), 200
